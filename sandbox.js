8548e830d7555301b7fe32817706bf571a90c1bd
unknown <mfatihcandan@gmail.com>
2021-02-24 14:48:16 +0300

    ts finance test

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var ninjas = [];
+var ninjaOne = { name: "Fatih", age: 24 };
+var ninjaTwo = { name: "Haydar", age: 30 };
+ninjas.push(ninjaOne);
+ninjas.push(ninjaTwo);
+console.log(ninjas);
---
+++ sandbox.ts
@@ -0,0 +1,8 @@
+let ninjas: object[]=[];
+
+let ninjaOne: object = { name:"Fatih", age:24 }
+let ninjaTwo: object = { name:"Haydar", age:30 }
+
+ninjas.push(ninjaOne);
+ninjas.push(ninjaTwo);
+console.log(ninjas)
